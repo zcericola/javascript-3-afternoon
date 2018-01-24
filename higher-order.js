8aca50e6d804cc7c5ba2fda9afe11a443f925270
Zac Cericola <zcericola@gmail.com>
2018-01-24 17:43:10 -0600

    finished higher order function problems

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers  = mixedNumbers.filter(function(e){
+  if(e % 2 === 0) return e;
+})
 
 
 
@@ -44,7 +46,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(e){
+  return e * 1.07;
+})
 
 
 
@@ -63,8 +67,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+let totalPopulation = populations.reduce(function(a,b){
+  return a + b;
+})
 
 
 ////////// PROBLEM 4 //////////
@@ -80,7 +85,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 {"monster":"Sandmush","CP":262},{"monster":"Sandmush","CP":25},{"monster":"Charaflier","CP":215},
 {"monster":"Ponylopse","CP":125},{"monster":"Bulbabunny","CP":178}];
 // Do not edit the code above.
-
+console.log(monstersInYourPocket[0]['CP']);
 /*
   Now we're going to ramp these up a little bit.
   Instead of just arrays of numbers, we are going to have array of objects that we want to use map, filter, and reduce with.
@@ -89,8 +94,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
+let myStrongest  = monstersInYourPocket.filter(function(m, index){  
+  if(m['CP'] > 200) return m;
+})
 
 
 ////////// PROBLEM 5 //////////
@@ -106,7 +112,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a high order method to get sum of all the order totals
 */
 
-let ordersTotal //Code Here
+let ordersTotal = orders.map(function(a){
+  return a['price'] * (a['tax'] + 1);
+})
+
+
 
 
 
@@ -126,6 +136,18 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.map(function(a){  
+  if(a.owner === 'Bob'){
+    return parseInt(a.price);    
+  }
+  
+}).filter(function(i){
+  return i != undefined;
+}).reduce(function(a,b){
+  return a + b;
+})
+
+
+
 
 
